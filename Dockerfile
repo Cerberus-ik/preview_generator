e328f31ce1b700412f21d5dbf2c043f379565b24
Philipp <philipphickisch@gmail.com>
2024-05-04 19:51:40 +0200

    Update

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+# Update system and install necessary libraries
+RUN apt-get update && apt-get install -y \
+    ffmpeg \
+    libsm6 \
+    libxext6 \
+    libpq-dev \
+    gcc
+
+COPY requirements.txt requirements.txt
+
+# Install Python dependencies
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
+
+COPY . .
+
+# Command to run the application
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+import time
+import cv2
+from tqdm import tqdm
+
+main_folder = os.getenv("ROOT_FOLDER")
+
+
+def walk_dirs():
+    for root, dirs, files in os.walk(main_folder):
+        for file in files:
+            if file.endswith(".mp4"):
+                video_file = os.path.join(root, file)
+                generate_preview_clip(video_file, root)
+
+
+def generate_preview_clip(video_file: str, output_dir: str, clip_count=10, clip_duration=3,
+                          output_resolution=(640, 360)):
+    if os.path.exists(os.path.join(output_dir, "preview.mp4")):
+        return
+    if os.path.exists(os.path.join(output_dir, "0.mp4")):
+        return
+    print(f"Generating preview for {video_file} to {output_dir}")
+    start_time: int = int(time.time())
+
+    frame_rate: int = get_video_frame_rate(video_file)
+    video_length: int = get_video_length(video_file)
+    for i in tqdm(range(clip_count)):
+        output_file: str = os.path.join(output_dir, f"{i}.mp4")
+        clip_start: int = int(video_length / clip_count * i)
+        subprocess.run(
+            [
+                "ffmpeg", "-hwaccel", "qsv", "-i", video_file, "-ss", str(clip_start), "-t", str(clip_duration),
+                "-vf", f"scale={output_resolution[0]}:{output_resolution[1]}", "-c:v", "h264_qsv", "-c:a", "aac",
+                output_file
+            ],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+
+    with open(os.path.join(output_dir, "concat.txt"), "w") as f:
+        for i in range(clip_count):
+            f.write(f"file '{i}.mp4'\n")
+
+    subprocess.run(
+        ["ffmpeg", "-hwaccel", "qsv", "-f", "concat", "-safe", "0", "-i", os.path.join(output_dir, "concat.txt"),
+         "-c:v", "h264_qsv", "-c:a", "copy", os.path.join(output_dir, "preview.mp4")],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+    )
+    for i in range(clip_count):
+        os.remove(os.path.join(output_dir, f"{i}.mp4"))
+
+    os.remove(os.path.join(output_dir, "concat.txt"))
+    end_time: int = int(time.time())
+    print(f"Preview generated in {end_time - start_time} seconds")
+
+
+def get_video_length(video_file: str) -> int:
+    cap = cv2.VideoCapture(video_file)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+    return round(frame_count / fps)
+
+
+def get_video_frame_rate(video_file: str) -> int:
+    cap = cv2.VideoCapture(video_file)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap.release()
+    return round(fps)
+
+
+if __name__ == "__main__":
+    walk_dirs()
